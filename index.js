790544cf4bc187b47d95be363728b02e90800346
github username <github email address>
2021-07-29 16:45:46 -0400

    commit tree 6fa18b756932b2c74b2dcb3efe62e628ba2e4946

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+// let newHeader=document.querySelector("h1#victory");
+// let newHeader =h1.document.createId('victory')
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML= ` ${name} is the champion`
